1e9c3adb5fd94c37b2b3366e5da51afcf50aef6b
Bryan Calisto <bryancalisto@yahoo.es>
2023-07-09 22:57:03 -0500

    Added random int function

--- ec-sri-invoice-sign/src/utils/utils.ts
+++ ec-sri-invoice-sign/src/utils/utils.ts
@@ -1,8 +1,9 @@
-const pemToDer = (pem: string) => {
-  return pem
-    .replace(/(\r\n|\r)/g, "\n")
-    .replace(/-----BEGIN [A-Z\x20]{1,48}-----\n?/, "")
-    .replace(/-----END [A-Z\x20]{1,48}-----\n?/, "");
+import * as crypto from 'crypto';
+
+const getRandomInt = (max: number = 10000) => {
+  return crypto.randomInt(max);
 }
 
-export { pemToDer };
+export {
+  getRandomInt
+};
---
+++ ec-sri-invoice-sign/test/utils.test.ts
@@ -0,0 +1,8 @@
+import { getRandomInt } from "../src/utils/utils";
+
+describe('Given the getRandomInt function', () => {
+  it('should return a random integer', () => {
+    const result = getRandomInt();
+    expect(typeof result === 'number' && !Number.isNaN(result) && result % 1 === 0).toEqual(true);
+  })
+});
