e5c4adfd71995cb6112edaeee16b7a19035d1642
Bryan Calisto <bryancalisto@yahoo.es>
2023-08-02 22:10:54 -0500

    attribute and namespace sorting is working

--- ec-sri-invoice-sign/src/canonicalization/c14n.ts
+++ ec-sri-invoice-sign/src/canonicalization/c14n.ts
@@ -1,23 +1,22 @@
 import { buildXml, parseXml } from "../utils/xml";
 
-const attributeCompare = (a: any, b: any) => {
+const attributeCompare = (a: Attribute, b: Attribute) => {
   if (!a.namespaceURI && b.namespaceURI) {
     return -1;
   }
+
   if (!b.namespaceURI && a.namespaceURI) {
     return 1;
   }
 
-  const left = a.namespaceURI + a.localName;
-  const right = b.namespaceURI + b.localName;
+  const left = a.namespaceURI + a.name;
+  const right = b.namespaceURI + b.name;
 
   if (left === right) {
     return 0;
-  } else if (left < right) {
-    return -1;
-  } else {
-    return 1;
   }
+
+  return left < right ? -1 : 1;
 }
 
 const namespaceCompare = (a: Namespace, b: Namespace) => {
@@ -33,18 +32,19 @@ const namespaceCompare = (a: Namespace, b: Namespace) => {
     return 0;
   }
 
-  return a.prefix.localeCompare(b.prefix);
+  return a.prefix < b.prefix ? -1 : 1;
 }
 
 interface Attribute {
   name: string;
-  namespace?: string;
+  namespacePrefix?: string;
+  namespaceURI?: string;
   value: string;
 }
 
 interface Namespace {
   prefix?: string;
-  value: string;
+  uri: string;
 }
 
 type Node = Record<string, any>;
@@ -53,7 +53,8 @@ type GenericCollection = Record<string, string>;
 
 const parseAttributesAndNamespaces = (data: GenericCollection) => {
   const attributes: Attribute[] = [];
-  const namespaces: Namespace[] = [];
+  const attributesWithPendingNamespace: Attribute[] = [];
+  const namespacesByPrefix: Record<string, Namespace> = {};
 
   for (const rawKey of Object.keys(data)) {
     const key = rawKey.substring(2); // without the @_ prefix
@@ -63,23 +64,58 @@ const parseAttributesAndNamespaces = (data: GenericCollection) => {
     const isAttributeWithNamespace = isAttribute && splittedKey.length === 2;
 
     if (isNamespace) {
-      namespaces.push({ prefix: splittedKey[1], value: data[rawKey] });
+      const prefix = splittedKey[1];
+      namespacesByPrefix[prefix] = { prefix, uri: data[rawKey] };
     }
 
     if (isAttribute) {
-      attributes.push({ name: splittedKey[isAttributeWithNamespace ? 1 : 0], namespace: isAttributeWithNamespace ? splittedKey[0] : undefined, value: data[rawKey] });
+      if (isAttributeWithNamespace) {
+        const prefix = splittedKey[0];
+        const namespaceURI = namespacesByPrefix[prefix]?.uri;
+
+        if (namespaceURI === undefined) {
+          attributesWithPendingNamespace.push({ name: splittedKey[isAttributeWithNamespace ? 1 : 0], namespaceURI, namespacePrefix: prefix, value: data[rawKey] });
+        }
+        else {
+          attributes.push({ name: splittedKey[isAttributeWithNamespace ? 1 : 0], namespacePrefix: prefix, namespaceURI, value: data[rawKey] });
+        }
+      }
+      else {
+        attributes.push({ name: splittedKey[isAttributeWithNamespace ? 1 : 0], namespacePrefix: undefined, namespaceURI: undefined, value: data[rawKey] });
+      }
     }
   }
 
-  return { attributes, namespaces };
+  // const solvedAttributesWithPendingNamespace: Attribute[] = attributesWithPendingNamespace.map((attr) => ({ name: attr.name, namespaceURI: namespacesByPrefix[attr.prefix].uri, value: attr.value }));
+  const solvedAttributesWithPendingNamespace: Attribute[] = attributesWithPendingNamespace.map((attr) => ({ ...attr, namespaceURI: namespacesByPrefix[attr.namespacePrefix ?? ''].uri, value: attr.value }));
+
+  return { attributes: [...attributes, ...solvedAttributesWithPendingNamespace], namespaces: Object.values(namespacesByPrefix) };
 }
 
 const sortNamespaces = (namespaces: Namespace[]) => {
   namespaces.sort(namespaceCompare);
 }
 
+const sortAttributes = (attributes: Attribute[]) => {
+  attributes.sort(attributeCompare);
+}
+
 const removeNode = (obj: Node[], currentPosition: number) => {
   obj.splice(currentPosition, 1);
+}
+
+const insertAttributesAndNamespaces = (node: Node, attributes: Attribute[], namespaces: Namespace[]) => {
+  const toInsert: GenericCollection = {};
+
+  namespaces.forEach((namespace) => {
+    toInsert[`@_xmlns${namespace.prefix ? `:${namespace.prefix}` : ''}`] = namespace.uri;
+  });
+
+  attributes.forEach((attr) => {
+    toInsert[`@_${attr.namespacePrefix ? `${attr.namespacePrefix}:${attr.name}` : attr.name}`] = attr.value;
+  });
+
+  node[':@'] = toInsert;
 }
 
 const processNode = (node: Node, depth: number) => {
@@ -87,10 +123,21 @@ const processNode = (node: Node, depth: number) => {
   const { attributes, namespaces } = parseAttributesAndNamespaces(node[':@'] ?? {})
 
   sortNamespaces(namespaces);
+  sortAttributes(attributes);
 
   const tagName = Object.keys(node).find((key) => !reservedKeywords.has(key));
   const children = (node[tagName!] ?? []) as Node[];
   let i = 0;
+
+  if (tagName === 'e5') {
+    console.log('NODE', node, 'ATTR', attributes, 'NAMESP', namespaces);
+  }
+
+  insertAttributesAndNamespaces(node, attributes, namespaces);
+
+  if (tagName === 'e5') {
+    console.log('AFTER NODE', node);
+  }
 
   while (i < children.length) {
     const child = children[i];
@@ -104,10 +151,6 @@ const processNode = (node: Node, depth: number) => {
 
     i++;
   }
-
-  console.log('NODE', node, attributes, namespaces);
-
-  // Convert node adding, relocating and removing things and return it
 }
 
 const processObj = (obj: Node[]) => {
@@ -130,8 +173,8 @@ const processObj = (obj: Node[]) => {
 
 const c14nCanonicalize = (xml: string) => {
   const obj = parseXml(xml);
+
   processObj(obj);
-  // console.log("OBJ", JSON.stringify(obj, null, 2));
 
   return buildXml(obj);
 }
@@ -139,266 +182,3 @@ const c14nCanonicalize = (xml: string) => {
 export {
   c14nCanonicalize
 }
-
-/*
-export class C14nCanonicalization implements CanonicalizationOrTransformationAlgorithm {
-  includeComments = false;
-
-  attrCompare(a, b) {
-    if (!a.namespaceURI && b.namespaceURI) {
-      return -1;
-    }
-    if (!b.namespaceURI && a.namespaceURI) {
-      return 1;
-    }
-
-    const left = a.namespaceURI + a.localName;
-    const right = b.namespaceURI + b.localName;
-
-    if (left === right) {
-      return 0;
-    } else if (left < right) {
-      return -1;
-    } else {
-      return 1;
-    }
-  }
-
-  nsCompare(a, b) {
-    const attr1 = a.prefix;
-    const attr2 = b.prefix;
-    if (attr1 === attr2) {
-      return 0;
-    }
-    return attr1.localeCompare(attr2);
-  }
-
-  renderAttrs(node) {
-    let i;
-    let attr;
-    const attrListToRender: Attr[] = [];
-
-    if (xpath.isComment(node)) {
-      return this.renderComment(node);
-    }
-
-    if (node.attributes) {
-      for (i = 0; i < node.attributes.length; ++i) {
-        attr = node.attributes[i];
-        //ignore namespace definition attributes
-        if (attr.name.indexOf("xmlns") === 0) {
-          continue;
-        }
-        attrListToRender.push(attr);
-      }
-    }
-
-    attrListToRender.sort(this.attrCompare);
-
-    const res = attrListToRender.map((attr) => {
-      return ` ${attr.name}="${utils.encodeSpecialCharactersInAttribute(attr.value)}"`;
-    });
-
-    return res.join("");
-  }
-
-  /**
-   * Create the string of all namespace declarations that should appear on this element
-   *
-   * @param node The node we now render
-   * @param prefixesInScope The prefixes defined on this node parents which are a part of the output set
-   * @param defaultNs The current default namespace
-   * @param defaultNsForPrefix
-   * @param ancestorNamespaces Import ancestor namespaces if it is specified
-   * @api private
-renderNs(
-  node: Element,
-  prefixesInScope: string[],
-  defaultNs: string,
-  defaultNsForPrefix: string,
-  ancestorNamespaces: NamespacePrefix[],
-): RenderedNamespace {
-  let i;
-  let attr;
-  const res: string[] = [];
-  let newDefaultNs = defaultNs;
-  const nsListToRender: { prefix: string; namespaceURI: string }[] = [];
-  const currNs = node.namespaceURI || "";
-
-  //handle the namespace of the node itself
-  if (node.prefix) {
-    if (prefixesInScope.indexOf(node.prefix) === -1) {
-      nsListToRender.push({
-        prefix: node.prefix,
-        namespaceURI: node.namespaceURI || defaultNsForPrefix[node.prefix],
-      });
-      prefixesInScope.push(node.prefix);
-    }
-  } else if (defaultNs !== currNs) {
-    //new default ns
-    newDefaultNs = node.namespaceURI || "";
-    res.push(' xmlns="', newDefaultNs, '"');
-  }
-
-  //handle the attributes namespace
-  if (node.attributes) {
-    for (i = 0; i < node.attributes.length; ++i) {
-      attr = node.attributes[i];
-
-      //handle all prefixed attributes that are included in the prefix list and where
-      //the prefix is not defined already. New prefixes can only be defined by `xmlns:`.
-      if (attr.prefix === "xmlns" && prefixesInScope.indexOf(attr.localName) === -1) {
-        nsListToRender.push({ prefix: attr.localName, namespaceURI: attr.value });
-        prefixesInScope.push(attr.localName);
-      }
-
-      //handle all prefixed attributes that are not xmlns definitions and where
-      //the prefix is not defined already
-      if (
-        attr.prefix &&
-        prefixesInScope.indexOf(attr.prefix) === -1 &&
-        attr.prefix !== "xmlns" &&
-        attr.prefix !== "xml"
-      ) {
-        nsListToRender.push({ prefix: attr.prefix, namespaceURI: attr.namespaceURI });
-        prefixesInScope.push(attr.prefix);
-      }
-    }
-  }
-
-  if (utils.isArrayHasLength(ancestorNamespaces)) {
-    // Remove namespaces which are already present in nsListToRender
-    for (const ancestorNamespace of ancestorNamespaces) {
-      let alreadyListed = false;
-      for (const nsToRender of nsListToRender) {
-        if (
-          nsToRender.prefix === ancestorNamespace.prefix &&
-          nsToRender.namespaceURI === ancestorNamespace.namespaceURI
-        ) {
-          alreadyListed = true;
-        }
-      }
-
-      if (!alreadyListed) {
-        nsListToRender.push(ancestorNamespace);
-      }
-    }
-  }
-
-  nsListToRender.sort(this.nsCompare);
-
-  //render namespaces
-  res.push(
-    ...nsListToRender.map((attr) => {
-      if (attr.prefix) {
-        return ` xmlns:${attr.prefix}="${attr.namespaceURI}"`;
-      }
-      return ` xmlns="${attr.namespaceURI}"`;
-    }),
-  );
-
-  return { rendered: res.join(""), newDefaultNs };
-}
-
-processInner(node, prefixesInScope, defaultNs, defaultNsForPrefix, ancestorNamespaces) {
-  if (xpath.isComment(node)) {
-    return this.renderComment(node);
-  }
-  if (node.data) {
-    return utils.encodeSpecialCharactersInText(node.data);
-  }
-
-  let i;
-  let pfxCopy;
-  const ns = this.renderNs(
-    node,
-    prefixesInScope,
-    defaultNs,
-    defaultNsForPrefix,
-    ancestorNamespaces,
-  );
-  const res = ["<", node.tagName, ns.rendered, this.renderAttrs(node), ">"];
-
-  for (i = 0; i < node.childNodes.length; ++i) {
-    pfxCopy = prefixesInScope.slice(0);
-    res.push(
-      this.processInner(node.childNodes[i], pfxCopy, ns.newDefaultNs, defaultNsForPrefix, []),
-    );
-  }
-
-  res.push("</", node.tagName, ">");
-  return res.join("");
-}
-
-// Thanks to deoxxa/xml-c14n for comment renderer
-renderComment(node: Comment) {
-  if (!this.includeComments) {
-    return "";
-  }
-
-  const isOutsideDocument = node.ownerDocument === node.parentNode;
-  let isBeforeDocument = false;
-  let isAfterDocument = false;
-
-  if (isOutsideDocument) {
-    let nextNode: ChildNode | null = node;
-    let previousNode: ChildNode | null = node;
-
-    while (nextNode !== null) {
-      if (nextNode === node.ownerDocument.documentElement) {
-        isBeforeDocument = true;
-        break;
-      }
-
-      nextNode = nextNode.nextSibling;
-    }
-
-    while (previousNode !== null) {
-      if (previousNode === node.ownerDocument.documentElement) {
-        isAfterDocument = true;
-        break;
-      }
-
-      previousNode = previousNode.previousSibling;
-    }
-  }
-
-  const afterDocument = isAfterDocument ? "\n" : "";
-  const beforeDocument = isBeforeDocument ? "\n" : "";
-  const encodedText = utils.encodeSpecialCharactersInText(node.data);
-
-  return `${afterDocument}<!--${encodedText}-->${beforeDocument}`;
-}
-
-/**
- * Perform canonicalization of the given node
- *
- * @param {Node} node
- * @return {String}
- * @api public
-process(node: Node, options: CanonicalizationOrTransformationAlgorithmProcessOptions) {
-  options = options || {};
-  const defaultNs = options.defaultNs || "";
-  const defaultNsForPrefix = options.defaultNsForPrefix || {};
-  const ancestorNamespaces = options.ancestorNamespaces || [];
-
-  const prefixesInScope: string[] = [];
-  for (let i = 0; i < ancestorNamespaces.length; i++) {
-    prefixesInScope.push(ancestorNamespaces[i].prefix);
-  }
-
-  const res = this.processInner(
-    node,
-    prefixesInScope,
-    defaultNs,
-    defaultNsForPrefix,
-    ancestorNamespaces,
-  );
-  return res;
-}
-
-getAlgorithmName() {
-  return "http://www.w3.org/TR/2001/REC-xml-c14n-20010315";
-}
-}
-*/
