73820a439f2b8dd2e315a027cfe77c7852f07953
Bryan Calisto <bryancalisto@yahoo.es>
2023-08-22 21:54:25 -0500

    Removed comment

--- ec-sri-invoice-sign/src/index.ts
+++ ec-sri-invoice-sign/src/index.ts
@@ -3,9 +3,3 @@ import { signInvoiceXml } from './signature/signature';
 export {
   signInvoiceXml
 };
-
-/**
- * TODO:
- * - Make sure the package can be used in other apps (exported correctly).
- * - Make sure the types are working fine on client code.
- */
