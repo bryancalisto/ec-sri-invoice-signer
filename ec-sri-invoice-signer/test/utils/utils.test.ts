2738c6ee35adfe91622c83055d3e3cfabd02c912
Bryan Calisto <bryancalisto@yahoo.es>
2023-09-15 09:36:32 -0500

    Fixed typo and added test for it

--- ec-sri-invoice-signer/test/utils/utils.test.ts
+++ ec-sri-invoice-signer/test/utils/utils.test.ts
@@ -1,5 +1,6 @@
-import { expect } from "chai";
-import Utils from "../../src/utils/utils";
+import { expect } from 'chai';
+import Utils from '../../src/utils/utils';
+import sinon, { SinonFakeTimers } from 'sinon';
 
 describe('Given the getRandomInt function', () => {
   it('should return a random UUID v4', () => {
@@ -9,8 +10,22 @@ describe('Given the getRandomInt function', () => {
 });
 
 describe('Given the getDate function', () => {
+  let clock: SinonFakeTimers;
+
+  afterEach(() => {
+    clock?.restore();
+  });
+
   it('should return the current date correctly formatted', () => {
     const result = Utils.getDate();
     expect(/^20\d{2}-[0-3]\d-[0-3]\dT[0-2]\d:[0-6]\d:[0-6]\d\.\d{3}[-+][0-2]\d:[0-6]\d$/.test(result)).to.be.true;
+  });
+
+  it('should return the current date in local time', () => {
+    clock = sinon.useFakeTimers({ now: 1694787394044 }); // 2023-09-15T14:16:34.044Z
+    const result = Utils.getDate();
+    // UTC-5. America/Guayaquil timezone is set in the server. Configure this locally if you are in a different timezone and
+    // want to run this test reliably
+    expect(result).to.equal('2023-09-15T09:16:34.044-05:00');
   });
 });
