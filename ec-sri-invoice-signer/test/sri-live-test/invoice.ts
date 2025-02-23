b9da8693eb9ca186bb05973abee0984c0500cea8
Bryan Calisto <bryancalisto@yahoo.es>
2025-02-23 07:33:23 -0500

    cleaning

--- ec-sri-invoice-signer/test/sri-live-test/invoice.ts
+++ ec-sri-invoice-signer/test/sri-live-test/invoice.ts
@@ -1,116 +1,8 @@
 import fs from 'fs';
 import { signInvoiceXml } from '../../src';
-import * as soap from 'soap';
-import readline from 'readline';
 import { XMLBuilder, XMLParser } from 'fast-xml-parser';
+import { findNode, getAccessKeyVerificationNumber, longPollDoc, sendDocToSRI } from './utils';
 
-/**
- * SRI test environment
- * https://celcer.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline?wsdl
- * https://celcer.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl
- */
-
-function waitForKeyPress() {
-  return new Promise((resolve) => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-
-    rl.question('[exit]: e, [continue]: any key, but not e', (answer) => {
-      if (answer === 'e') {
-        process.exit(0);
-      }
-      else {
-        resolve(true);
-        rl.close();
-      }
-    });
-  });
-}
-
-async function sendInvoiceToSRI(signedInvoice: string) {
-  const url = 'https://celcer.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline?wsdl';
-
-  return new Promise((resolve, reject) => {
-    soap.createClient(url, { wsdl_options: { timeout: 5000 } }, function (err, client) {
-      if (err) {
-        reject(err);
-      } else {
-        const args = { xml: Buffer.from(signedInvoice).toString('base64') };
-
-        console.log('sending invoice to SRI servers...');
-
-        client.validarComprobante(args, function (err: any, result: any) {
-          if (err) {
-            console.log('error while sending invoice to SRI servers:', err);
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        });
-      }
-    });
-  });
-}
-
-async function checkInvoiceAuthorization(accessKey: string) {
-  const url = 'https://celcer.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl';
-
-
-  return new Promise((resolve, reject) => {
-    soap.createClient(url, { wsdl_options: { timeout: 5000 } }, function (err, client) {
-      if (err) {
-        reject(err);
-      } else {
-        const args = { claveAccesoComprobante: accessKey };
-        client.autorizacionComprobante(args, function (err: any, result: any) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        });
-      }
-    });
-  });
-}
-
-function getAccessKeyVerificationNumber(data: string) {
-  let sum = 0;
-  let factor = 7;
-
-  for (let i = 0; i < data.length; i++) {
-    sum += Number(data[i]) * factor;
-    factor--;
-    if (factor === 1) {
-      factor = 7;
-    }
-  }
-
-  const result = 11 - (sum % 11);
-
-  const verificatioNumber = (result === 11 ? 0 : result === 10 ? 1 : result).toString();
-  return verificatioNumber;
-}
-
-async function longPollInvoice({ accessKey }: { accessKey: string }) {
-  while (true) {
-    await waitForKeyPress();
-    const result = await checkInvoiceAuthorization(accessKey);
-    console.log('invoice status:', JSON.stringify(result, null, 2));
-  }
-}
-
-function findNode(tagName: string, tree: any) {
-  for (const node of tree) {
-    if (tagName in node) {
-      return node[tagName];
-    }
-  }
-
-  throw new Error(`Node ${tagName} not found`);
-}
 
 async function main() {
   // General params: adjust according to your setup
@@ -201,12 +93,12 @@ ${numericCode}${emissionType}`;
   fs.writeFileSync('debug-signed.xml', signedInvoice);
 
   console.log('sending invoice to SRI servers...');
-  const result = await sendInvoiceToSRI(signedInvoice);
+  const result = await sendDocToSRI(signedInvoice);
   console.log('invoice sent to SRI servers:', JSON.stringify(result, null, 2));
 
   // Poll until invoice has been processed
   console.log('polling for invoice information...');
-  await longPollInvoice({ accessKey: accessKeyWithVerificationNumber });
+  await longPollDoc({ accessKey: accessKeyWithVerificationNumber });
 }
 
 main()
---
+++ ec-sri-invoice-signer/test/sri-live-test/utils.ts
@@ -0,0 +1,110 @@
+import * as soap from 'soap';
+import readline from 'readline';
+
+/**
+ * SRI test environment
+ * https://celcer.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline?wsdl
+ * https://celcer.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl
+ */
+
+function waitForKeyPress() {
+  return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question('[exit]: e, [continue]: any key, but not e', (answer) => {
+      if (answer === 'e') {
+        process.exit(0);
+      }
+      else {
+        resolve(true);
+        rl.close();
+      }
+    });
+  });
+}
+
+export async function sendDocToSRI(signedInvoice: string) {
+  const url = 'https://celcer.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline?wsdl';
+
+  return new Promise((resolve, reject) => {
+    soap.createClient(url, { wsdl_options: { timeout: 5000 } }, function (err, client) {
+      if (err) {
+        reject(err);
+      } else {
+        const args = { xml: Buffer.from(signedInvoice).toString('base64') };
+
+        console.log('sending invoice to SRI servers...');
+
+        client.validarComprobante(args, function (err: any, result: any) {
+          if (err) {
+            console.log('error while sending invoice to SRI servers:', err);
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      }
+    });
+  });
+}
+
+async function checkDocAuthorization(accessKey: string) {
+  const url = 'https://celcer.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl';
+
+
+  return new Promise((resolve, reject) => {
+    soap.createClient(url, { wsdl_options: { timeout: 5000 } }, function (err, client) {
+      if (err) {
+        reject(err);
+      } else {
+        const args = { claveAccesoComprobante: accessKey };
+        client.autorizacionComprobante(args, function (err: any, result: any) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      }
+    });
+  });
+}
+
+export function getAccessKeyVerificationNumber(data: string) {
+  let sum = 0;
+  let factor = 7;
+
+  for (let i = 0; i < data.length; i++) {
+    sum += Number(data[i]) * factor;
+    factor--;
+    if (factor === 1) {
+      factor = 7;
+    }
+  }
+
+  const result = 11 - (sum % 11);
+
+  const verificatioNumber = (result === 11 ? 0 : result === 10 ? 1 : result).toString();
+  return verificatioNumber;
+}
+
+export async function longPollDoc({ accessKey }: { accessKey: string }) {
+  while (true) {
+    await waitForKeyPress();
+    const result = await checkDocAuthorization(accessKey);
+    console.log('doc status:', JSON.stringify(result, null, 2));
+  }
+}
+
+export function findNode(tagName: string, tree: any) {
+  for (const node of tree) {
+    if (tagName in node) {
+      return node[tagName];
+    }
+  }
+
+  throw new Error(`Node ${tagName} not found`);
+}
