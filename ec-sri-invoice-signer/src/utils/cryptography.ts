3a7231c5296dbc02a3f1d72d9657b42c6992aece
bryancalisto <bryancalisto@yahoo.es>
2026-07-07 11:30:46 -0500

    fix exponent and modulus extraction and refactor some stuff to simplify and be consistent with forge lib usage

--- ec-sri-invoice-signer/src/utils/cryptography.ts
+++ ec-sri-invoice-signer/src/utils/cryptography.ts
@@ -2,71 +2,98 @@ import * as forge from 'node-forge';
 import { UnsuportedPkcs12Error } from './errors';
 
 const sign = (data: string, privateKey: forge.pki.rsa.PrivateKey) => {
-  return forge.util.encode64(privateKey.sign(forge.sha1.create().update(data, 'utf8')));
+  const md = forge.md.sha1.create();
+  md.update(data, 'utf8');
+  return forge.util.encode64(privateKey.sign(md));
 }
 
 const getHash = (data: string) => {
-  return forge.util.encode64(forge.sha1.create().update(data, 'utf8').digest().bytes());
+  const md = forge.md.sha1.create();
+  md.update(data, 'utf8');
+  return forge.util.encode64(md.digest().bytes());
 }
 
 const getBancoCentralPkcs12PrivateKey = (pkcs8ShroudedKeyBags: forge.pkcs12.Bag[]) => {
-    const privateKeyBag = pkcs8ShroudedKeyBags.find((bag) => {
-      const name = bag?.attributes?.friendlyName?.[0];
-      return /signing key/i.test(name);
-    });
+  const privateKeyBag = pkcs8ShroudedKeyBags.find((bag) => {
+    const name = bag?.attributes?.friendlyName?.[0];
+    return /signing|private|key/i.test(name) || !name;
+  });
 
-    if (!privateKeyBag) {
-      throw new UnsuportedPkcs12Error("No key bag with friendly name 'Signing Key' found in the key bags of 'Banco Central del Ecuador' .p12");
-    }
+  if (!privateKeyBag) {
+    throw new UnsuportedPkcs12Error("No private key bag found in BCE .p12");
+  }
 
-    const privateKey = privateKeyBag.key as forge.pki.rsa.PrivateKey;
+  const privateKey = privateKeyBag.key as forge.pki.rsa.PrivateKey;
 
-    if (!privateKey) {
-      throw new UnsuportedPkcs12Error("No valid key found in 'Banco Central del Ecuador' .p12");
-    }
+  if (!privateKey) {
+    throw new UnsuportedPkcs12Error("No valid key found in BCE .p12");
+  }
 
-    return privateKey;
+  return privateKey;
 }
 
-const extractPrivateKeyAndCertificateFromPkcs12 = (pkcs12RawData: string | Buffer, password: string = '') => {
-  const pkcs12InBase64 = typeof pkcs12RawData === 'string' ? pkcs12RawData : pkcs12RawData.toString('base64');
+const extractPrivateKeyAndCertificateFromPkcs12 = (
+  pkcs12RawData: string | Buffer, 
+  password: string = ''
+) => {
+  const pkcs12InBase64 = typeof pkcs12RawData === 'string' 
+    ? pkcs12RawData 
+    : pkcs12RawData.toString('base64');
+  
   const pkcs12InDer = forge.util.decode64(pkcs12InBase64);
   const p12Asn1 = forge.asn1.fromDer(pkcs12InDer);
   const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, password);
+  
   const certBags = p12.getBags({ bagType: forge.pki.oids.certBag });
   const certBag = certBags[forge.pki.oids.certBag]?.[0];
   const pkcs8ShroudedKeyBags = p12.getBags({ bagType: forge.pki.oids.pkcs8ShroudedKeyBag });
 
-  if (!certBag || !pkcs8ShroudedKeyBags) {
-    throw new UnsuportedPkcs12Error();
+  if (!certBag) {
+    throw new UnsuportedPkcs12Error("No certificate found in PKCS#12");
+  }
+
+  if (!pkcs8ShroudedKeyBags || Object.keys(pkcs8ShroudedKeyBags).length === 0) {
+    throw new UnsuportedPkcs12Error("No private key found in PKCS#12");
   }
 
   const friendlyName = certBag?.attributes?.friendlyName?.[0];
-
   const certificate = certBag.cert;
 
   if (!certificate) {
-    throw new UnsuportedPkcs12Error("Couldn't find its certificate");
+    throw new UnsuportedPkcs12Error("Couldn't find certificate");
   }
 
   let privateKey: forge.pki.rsa.PrivateKey | null = null;
 
-  if (/banco central/i.test(friendlyName)) {
-    privateKey = getBancoCentralPkcs12PrivateKey(pkcs8ShroudedKeyBags[forge.pki.oids.pkcs8ShroudedKeyBag] ?? []);
-  }
-  else {
-    const firstPkcs8ShroudedKeyBag = pkcs8ShroudedKeyBags[forge.pki.oids.pkcs8ShroudedKeyBag]?.[0];
-    privateKey = firstPkcs8ShroudedKeyBag?.key ? firstPkcs8ShroudedKeyBag.key as forge.pki.rsa.PrivateKey : null;
+  // Extract private key
+  const allKeyBags = [
+    ...(pkcs8ShroudedKeyBags[forge.pki.oids.pkcs8ShroudedKeyBag] || []),
+    ...(pkcs8ShroudedKeyBags[forge.pki.oids.keyBag] || []),
+  ];
+
+  // If it's a BCE certificate, use the specific extraction
+  if (/banco central/i.test(friendlyName) || /eci|bce/i.test(friendlyName)) {
+    privateKey = getBancoCentralPkcs12PrivateKey(allKeyBags);
+  } else {
+    const firstKeyBag = allKeyBags[0];
+    privateKey = firstKeyBag?.key ? firstKeyBag.key as forge.pki.rsa.PrivateKey : null;
   }
 
   if (!privateKey) {
-    throw new UnsuportedPkcs12Error("Couldn't find its private key");
+    throw new UnsuportedPkcs12Error("Couldn't find private key");
   }
+
+  // Also extract the full certificate chain if available
+  const caBags = p12.getBags({ bagType: forge.pki.oids.certBag });
+  const certChain = (caBags[forge.pki.oids.certBag] || [])
+    .map(bag => bag.cert)
+    .filter(cert => cert !== undefined);
 
   return {
     privateKey,
-    certificate
-  }
+    certificate,
+    certChain // Full chain for validation
+  };
 }
 
 const normalizeIssuerAttributeShortName = (shortName: string) => {
@@ -82,17 +109,16 @@ const normalizeIssuerAttributeShortName = (shortName: string) => {
 };
 
 const extractIssuerData = (certificate: forge.pki.Certificate) => {
-  const issuerName = certificate.issuer.attributes.reverse().filter((attr) => attr.shortName || attr.type).map((attr) => {
-    if (attr.shortName) {
-      const normalizedShortName = normalizeIssuerAttributeShortName(attr.shortName);
-      return `${normalizedShortName}=${attr.value}`;
-    }
-    else {
-      return `${attr.type}=${attr.value}`;
-    }
-  }).join(','); // .reverse to follow convention of country-name-is-last seen in the SRI example
+   // reverse to follow convention of country-name-is-last seen in the SRI example
+  const attributes = certificate.issuer.attributes
+    .filter((attr) => attr.shortName || attr.type)
+    .reverse();
 
-  return issuerName;
+  return attributes.map((attr) => {
+    const name = attr.shortName ? normalizeIssuerAttributeShortName(attr.shortName) : attr.type;
+    const value = attr.value || '';
+    return `${name}=${value}`;
+  }).join(',');
 }
 
 const extractX509Data = (certificate: forge.pki.Certificate) => {
@@ -112,8 +138,12 @@ const extractX509Data = (certificate: forge.pki.Certificate) => {
 }
 
 const extractPrivateKeyData = (privateKey: forge.pki.rsa.PrivateKey) => {
-  const modulus = Buffer.from(privateKey.n.toString(), 'hex').toString('base64');
-  const exponent = Buffer.from(privateKey.e.toString(), 'hex').toString('base64');
+  const modulus = forge.util.encode64(
+    forge.util.hexToBytes(privateKey.n.toString(16))
+  );
+  const exponent = forge.util.encode64(
+    forge.util.hexToBytes(privateKey.e.toString(16))
+  );
 
   return {
     modulus,
